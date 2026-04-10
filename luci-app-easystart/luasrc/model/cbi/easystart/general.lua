10d277465bb304cae1718c9270ea67f180bc4145
qilirong <1079894799@qq.com>
2026-04-11 00:21:21 +0800

    修复 CBI 文件中的变量名冲突

--- luci-app-easystart/luasrc/model/cbi/easystart/general.lua
+++ luci-app-easystart/luasrc/model/cbi/easystart/general.lua
@@ -38,50 +38,50 @@ end
 mode = m:section(TypedSection, 'general', _('模式选择'))
 mode.anonymous = true
 
-mode_type = mode:option(ListValue, 'type', _('工作模式'))
+mode_type = mode:option(ListValue, 'mode', _('工作模式'))
 mode_type:value('main', _('传统路由模式（主路由）'))
 mode_type:value('bypass', _('旁路由模式'))
 mode_type:value('bridge', _('桥接模式（AP/交换机）'))
 mode_type.default = 'main'
 
 -- 传统路由模式参数
-main_proto = mode:option(ListValue, 'main_proto', _('上网方式'))
-main_proto:depends('type', 'main')
-main_proto:value('dhcp', _('动态 IP'))
-main_proto:value('pppoe', _('PPPoE 拨号'))
-main_proto:value('static', _('静态 IP'))
-main_proto.default = 'dhcp'
+local proto = mode:option(ListValue, 'proto', _('上网方式'))
+proto:depends('mode', 'main')
+proto:value('dhcp', _('动态 IP'))
+proto:value('pppoe', _('PPPoE 拨号'))
+proto:value('static', _('静态 IP'))
+proto.default = 'dhcp'
 
 pppoe_username = mode:option(Value, 'pppoe_username', _('PPPoE 账号'))
-pppoe_username:depends('main_proto', 'pppoe')
+pppoe_username:depends('proto', 'pppoe')
 
 pppoe_password = mode:option(Value, 'pppoe_password', _('PPPoE 密码'))
-pppoe_password:depends('main_proto', 'pppoe')
+pppoe_password:depends('proto', 'pppoe')
 pppoe_password.password = true
 
 static_ip = mode:option(Value, 'static_ip', _('静态 IP 地址'))
-static_ip:depends('main_proto', 'static')
+static_ip:depends('proto', 'static')
 static_ip.default = '192.168.1.1'
 
 static_netmask = mode:option(Value, 'static_netmask', _('子网掩码'))
-static_netmask:depends('main_proto', 'static')
+static_netmask:depends('proto', 'static')
 static_netmask.default = '255.255.255.0'
 
 static_gateway = mode:option(Value, 'static_gateway', _('默认网关'))
-static_gateway:depends('main_proto', 'static')
+static_gateway:depends('proto', 'static')
 static_gateway.default = '192.168.1.1'
 
 static_dns = mode:option(Value, 'static_dns', _('DNS 服务器'))
-static_dns:depends('main_proto', 'static')
+static_dns:depends('proto', 'static')
 static_dns.default = '114.114.114.114'
 
 -- 旁路由模式参数
 bypass_ip = mode:option(Value, 'bypass_ip', _('旁路由 IP 地址'))
-bypass_ip:depends('type', 'bypass')
+bypass_ip:depends('mode', 'bypass')
 bypass_ip.default = '192.168.1.2'
 
 bypass_gateway = mode:option(Value, 'bypass_gateway', _('主路由 IP 地址'))
-bypass_gateway:depends('type', 'bypass')
+bypass_gateway:depends('mode', 'bypass')
 bypass_gateway.default = '192.168.1.1'
 
 -- 应用按钮
@@ -91,7 +91,7 @@ apply.inputstyle = 'apply'
 
 function apply.write(self, section, value)
     local mode = mode_type:formvalue(section)
-    local proto = main_proto:formvalue(section)
+    local proto_val = proto:formvalue(section)
     local username = pppoe_username:formvalue(section)
     local password = pppoe_password:formvalue(section)
     local ipaddr = ''
@@ -100,7 +100,7 @@ function apply.write(self, section, value)
     local dns = ''
     
     if mode == 'main' then
-        if proto == 'static' then
+        if proto_val == 'static' then
             ipaddr = static_ip:formvalue(section)
             netmask = static_netmask:formvalue(section)
             gateway = static_gateway:formvalue(section)
@@ -113,7 +113,7 @@ function apply.write(self, section, value)
     
     -- 调用后端脚本
     local url = '/cgi-bin/luci/admin/easystart/apply'
-    local data = { mode = mode, proto = proto, username = username, password = password, ipaddr = ipaddr, netmask = netmask, gateway = gateway, dns = dns }
+    local data = { mode = mode, proto = proto_val, username = username, password = password, ipaddr = ipaddr, netmask = netmask, gateway = gateway, dns = dns }
     
     luci.http.redirect(url .. '?' .. luci.http.build_query(data))
 end
